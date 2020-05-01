844594ef18a9a5a4638d9e25aad0ef42762aa1f0
Bruno Ricardo <brunoric3d@gmail.com>
2020-04-30 22:30:56 -0300

    Finalização do desafio

---
+++ src/database/migrations/1588041892536-CreateTransactionsCategoriesRelation.ts
@@ -0,0 +1,22 @@
+import { MigrationInterface, QueryRunner, TableForeignKey } from 'typeorm';
+
+export default class CreateTransactionsCategoriesRelation1588041892536
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createForeignKey(
+      'transactions',
+      new TableForeignKey({
+        name: 'TransactionCategories',
+        columnNames: ['category_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'categories',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('transactions', 'TransactionCategories');
+  }
+}
